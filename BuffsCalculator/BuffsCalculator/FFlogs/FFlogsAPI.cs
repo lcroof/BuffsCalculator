0e4853ffbc2356d4b367411ccf507ef36d0c5f8d
lcroof <lcroof@qq.com>
2021-10-15 00:54:01 +0000

    test update

--- BuffsCalculator/BuffsCalculator/FFlogs/FFlogsAPI.cs
+++ BuffsCalculator/BuffsCalculator/FFlogs/FFlogsAPI.cs
@@ -16,6 +16,7 @@ namespace BuffsCalculator
         public static string GetAllResoucesByAbilityID(string fightCode, string startTime, string endTime, decimal abilityID)
         {
             string jsonString = string.Empty;
+            string a = string.Empty;
             return jsonString;
         }
 
